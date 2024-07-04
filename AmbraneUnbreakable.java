08051931a5bdd10d052e5770265ff5c4f4939cae
Naman Chauhan <namanchauhan.xworkz@gmail.com>
2024-07-04 10:44:26 +0530

    Variables and datatypes with using static

---
+++ AmbraneUnbreakable.java
@@ -0,0 +1,16 @@
+class AmbraneUnbreakable{
+	
+	public static void main(String[] charger){
+		
+		System.out.println(" 3A Fast Charging - Delivers 3A fast charging and 480 Mbps data transfer speed. ");
+		System.out.println(" Quick Charge 2.0/ 3.0 - Supports Quick Charge (QC) 2.0/3.0 to deliver rapid charging speed. ");
+		System.out.println(" Wide Compatibility - It is compatible with all Type-C enabled devices including Android smartphones, tablets and PC peripherals. ");
+		System.out.println(" Unbreakable - Made of special braided outer with rugged interior bindings, it is an ultra-durable cable that won’t be affected by daily rough usage. ");
+		System.out.println(" Ideal Length - It has an ideal length of 1.5 meters which is super convenient to use. ");
+		System.out.println(" Made in India - This product is Made in India at the Ambrane’s manufacturing facility in Haryana. ");
+		System.out.println(" 180 Days Warranty - It comes with a 6 months warranty against manufacturing defects. ");
+		
+	}
+	
+	
+}

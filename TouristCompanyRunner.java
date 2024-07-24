bcab0b64ec8a4fce6409fc63ee681d7435a9c967
Naman Chauhan <namanchauhan.xworkz@gmail.com>
2024-07-24 20:39:26 +0530

    Task given on 24-07-24

---
+++ TouristCompanyRunner.java
@@ -0,0 +1,50 @@
+class TouristCompanyRunner{
+ public static void main(String[] places){
+ 
+   boolean addedPlace = TouristCompany.addTouristPlace("Dubai");
+   System.out.println("Added Places " + addedPlace );
+   
+   TouristCompany.addTouristPlace("Singapore");
+   System.out.println("Added Places " + addedPlace );
+   
+   TouristCompany.addTouristPlace("USA");
+   System.out.println("Added Places " + addedPlace );
+   
+   TouristCompany.addTouristPlace("Paris");
+   System.out.println("Added Places " + addedPlace );
+   
+   TouristCompany.addTouristPlace("China");
+   System.out.println("Added Places " + addedPlace );
+   
+   TouristCompany.addTouristPlace("Korea");
+   System.out.println("Added Places " + addedPlace );
+   
+   TouristCompany.addTouristPlace("Ireland");
+   System.out.println("Added Places " + addedPlace );
+   
+   TouristCompany.addTouristPlace("Sweden");
+   System.out.println("Added Places " + addedPlace );
+   
+   TouristCompany.addTouristPlace("Pakistan");
+   System.out.println("Added Places " + addedPlace );
+   
+   TouristCompany.addTouristPlace("Russia");
+   System.out.println("Added Places " + addedPlace );
+   
+   TouristCompany.addTouristPlace("Turkey");
+   System.out.println("Added Places " + addedPlace );
+   
+   TouristCompany.addTouristPlace("Maldives");
+   System.out.println("Added Places " + addedPlace );
+  
+   
+   
+   
+   TouristCompany.showTouristNames();
+ 
+ }
+
+
+
+
+}
